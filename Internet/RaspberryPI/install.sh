28b4568012732f8dc5eb52febfa8d90b22959c53
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-05-11 16:19:47 -0700

    Update install.sh

--- Internet/RaspberryPI/install.sh
+++ Internet/RaspberryPI/install.sh
@@ -1,6 +1,10 @@
+echo "Installing"
+
 sudo apt-get update
-sudo apt-get install python3-pip
+sudo apt-get -y install python3-pip
+echo "installing Beautiful Soup"
 pip3 install bs4
 pip3 install beautifulsoup4
-
+echo "Instaling Requests" 
 python -m pip install requests
+echo "All Done"
