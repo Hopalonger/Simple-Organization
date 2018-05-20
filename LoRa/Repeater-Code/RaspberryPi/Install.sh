fdca60b2f3cde4621f78b93bc40ae379a8642af2
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-05-20 16:06:44 -0700

    Update Install.sh

--- LoRa/Repeater-Code/RaspberryPi/Install.sh
+++ LoRa/Repeater-Code/RaspberryPi/Install.sh
@@ -8,4 +8,6 @@ sudo apt-get -y install python3-bs4
 echo "Instaling Requests" 
 python -m pip install requests
 sudo apt-get -y install libnss-mdns
+sudo update-rc.d Simple-Organization/LoRa/Repeater-Code/RaspberryPi/Start.sh defaults
+chmod +x Simple-Organization/LoRa/Repeater-Code/RaspberryPi/Start.sh
 echo "All Done"
