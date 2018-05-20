aa9d9ddb28e7e6585a2acd3e20f8624b8e6f0a89
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-05-20 16:30:47 -0700

    Update install.sh

--- Server/install.sh
+++ Server/install.sh
@@ -1,25 +1,29 @@
-
+print "[+] Installing updates"
 sudo apt update -y
+print "[+] Installing upgrades"
 sudo apt upgrade -y 
+print "[+] Installing updates
 sudo apt update -y 
+print "[+] Installing Webserver"
 sudo apt -y  install apache2
 sudo apt -y  install php php-mbstring
 sudo apt -y  install mysql-server php-mysql
 sudo apt-get install php5-curl
 sudo apt-get -y install git 
 sudo apt-get -y install avahi-utils
+print "[+] Installing Website"
+sudo rm /var/www/html/index.html 
 git clone https://github.com/Hopalonger/opencart.git 
-sudo cp master.zip /var/www/html/
-sudo cd /var/www/html
-sudo unzip master.zip
-sudo cd opencart-master
-sudo mv -v upload/* ../opencart-master/\
-sudo cp /var/www/html/opencart-master/admin/config-dist.php /var/www/html/opencart-master/admin/config.php
-sudo cp /var/www/html/opencart-master/config-dist.php /var/www/html/opencart-master/config.php
+sudo cp -r opencart/upload /var/www/html/
+cd /var/www/html
+
+sudo
+udo cp /var/www/html/admin/config-dist.php /var/www/html/admin/config.php
+sudo cp /var/www/html/config-dist.php /var/www/html/config.php
 sudo chmod 777 -R /var/www/html/opencart-master 
 
 // Setting To Run At Startup 
-
+print "[+] Setting Auto Start Run"
 sudo update-rc.d Simple-Organization/Server/Start.sh defaults
 chmod +x Simple-Organization/Server/Start.sh
 print "Create Database and remove install. Once Completed Restart"
