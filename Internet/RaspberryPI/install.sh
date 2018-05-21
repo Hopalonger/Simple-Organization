752289d8e32a8899bc64d1e3c695860266b5e97e
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-05-21 16:57:59 -0700

    Updates To Require Less hardware

--- Internet/RaspberryPI/install.sh
+++ Internet/RaspberryPI/install.sh
@@ -1,11 +1,18 @@
-echo "Installing"
-
+echo "Installing Prerequisite"
+sudo apt-get install build-essential python-dev git scons swig
 sudo apt-get update
 sudo apt-get -y install python3-pip
-echo "installing Beautiful Soup"
+echo "[+] Installing Beautiful Soup"
 pip3 install bs4
 pip3 install beautifulsoup4
 sudo apt-get -y install python3-bs4
-echo "Instaling Requests" 
+echo "[+] Instaling Requests" 
 python -m pip install requests
+echo "[+] Installing NeoPixels"
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+scons
+echo "[+] Installing Python Update"
+cd python
+sudo python setup.py install
 echo "All Done"
