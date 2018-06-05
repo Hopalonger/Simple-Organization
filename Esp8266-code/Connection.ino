055c0e1af1161b00694aecca12d9f3d6d2c2f51a
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-06-04 22:08:01 -0700

    Fixed Format
    Fixed Format And typos Still Work in progress but Should be usable

--- Esp8266-code/Connection.ino
+++ Esp8266-code/Connection.ino
@@ -1,5 +1,4 @@
-#include <mdns.h>
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>          // Required Esp8266 Library's For functionality
 #include <ESP8266WiFiAP.h>
 #include <ESP8266WiFiGeneric.h>
 #include <ESP8266WiFiMulti.h>
@@ -11,20 +10,18 @@
 #include <WiFiServer.h>
 #include <WiFiServerSecure.h>
 #include <WiFiUdp.h>
-
 #include <ESP8266HTTPClient.h>
 
-#include <mDNSResolver.h>          https://github.com/madpilot/mDNSResolver
-
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <mDNSResolver.h>          //https://github.com/madpilot/mDNSResolver Also under the Libary manager under as esp8266_mDNS
+#include <ESP8266WiFi.h>          //https://github.com/tzapu/WiFiManager Also under the Libary manager under as WifiManager
 #include <Adafruit_NeoPixel.h>
-//needed for library
+//needed for library Wifi Manager
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 //*******************************************************Settings*************************************************
 // Change to the Server Hostname
-#define HOSTNAME      "Simple.local"
+#define HOSTNAME      "raspberrypi-2.local"
 // Color
 #define R 255
 #define G 255
@@ -33,15 +30,58 @@
 #define LED 60
 // Led Connected Pin
 #define PIN 6
+// Indicator Pin
+#define IND 8
+// Indicator LEDs
+#define LND 8
 // Device Code
 #define Code "aaaa"
 //*******************************************************Settings End*********************************************
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED, PIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel ind = Adafruit_NeoPixel(LND, IND, NEO_GRB + NEO_KHZ400);
 IPAddress   ip = 1;
 using namespace mDNSResolver;
 WiFiClient wifiClient;
 WiFiUDP udp;
 Resolver resolver(udp);
+void Indicator() {
+  int count = 0;
+  while (count < LND) {
+    ind.setPixelColor(count, 255, 255, 255);
+    count++;
+  }
+
+}
+void Start() {
+  int count = 0;
+  while (count < LND) {
+    strip.setPixelColor(count, 255, 255, 255);
+    count++;
+  }
+  count = 0;
+  while (count < LND) {
+    ind.setPixelColor(count, 255, 255, 255);
+    count++;
+  }
+  ind.show();
+
+  strip.show();
+  delay(5000);
+  count = 0;
+  while (count < LND) {
+    strip.setPixelColor(count, 0, 0, 0);
+    count++;
+  }
+
+  count = 0;
+  while (count < LND) {
+    ind.setPixelColor(count, 0, 0, 0);
+    count++;
+  }
+  ind.show();
+  strip.show();
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -59,7 +99,7 @@ void setup() {
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
   //and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("AutoConnectAP");
+  wifiManager.autoConnect("Organization");
   //or use this for auto generated name ESP + ChipID
   //wifiManager.autoConnect();
 
@@ -67,53 +107,73 @@ void setup() {
   //if you get here you have connected to the WiFi
   Serial.println("Connected to Internet.");
   Serial.println("Running Light Test");
+  delay(2000);
+  if (WiFi.status() == WL_CONNECTED){
 
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Resolving ");
+  Serial.println(HOSTNAME);
+  
   resolver.setLocalIP(WiFi.localIP());
+  
   IPAddress   ip = resolver.search(HOSTNAME);
   if (ip != INADDR_NONE) {
     Serial.print("Resolved: ");
     Serial.println(ip);
+    
   } else {
     Serial.println("Not resolved");
+    Serial.println(ip);
   }
   Serial.print("Scanning for Device Code:");
   Serial.println(Code);
-
+  }
 }
 
 void loop() {
-  String ipString = String(ip[0]);
-  for (byte octet = 1; octet < 4; ++octet) {
-    ipString += '.' + String(ip[octet]);
-  }
-
-  // put your main code here, to run repeatedly:
-  HTTPClient http;  //Declare an object of class HTTPClient
-  http.begin(ipString);  //Specify request destination
-
-  int httpCode = http.GET();
-  if (httpCode > 0) { //Check the returning code
-
-    String payload = http.getString();
-    Serial.print("Received: '");
-    Serial.println(payload);
-    String Data = payload;
-    String Data4 = Data.substring(0, 4);
-    Serial.print("Received Device Code:");
-    Serial.println(Data.substring(0, 4));
-    if (Data4 == Code) {
-      Serial.print("Recived LED:");
-      String LEDs = Data.substring(4);
-      Serial.println(LED);
-      Data = "";
-      Data4 = "";
-      int l = LEDs.toInt();
-      strip.setPixelColor(l, R, G, B);
+  while (WiFi.status() == WL_CONNECTED ) {
+    String ipString = String(ip[0]);
+    for (byte octet = 1; octet < 4; ++octet) {
+      ipString += '.' + String(ip[octet]);
     }
-    else {
-      Serial.println("HTTP Not Working");
+    String Ip2 = "http://" + ip;
+    // put your main code here, to run repeatedly:
+    HTTPClient http;  //Declare an object of class HTTPClient
+    http.begin( Ip2 + "/newfile.txt");  //Specify request destination
 
+    int httpCode = http.GET();
+    if (httpCode > 0) { //Check the returning code
+
+      String payload = http.getString();
+      Serial.print("Received: '");
+      Serial.println(payload);
+      String Data = payload;
+      String Data4 = Data.substring(0, 4);
+      Serial.print("Received Device Code:");
+      Serial.println(Data.substring(0, 4));
+      if (Data4 == Code) {
+        Serial.print("Recived LED:");
+        String LEDs = Data.substring(4);
+        Serial.println(LED);
+        Data = "";
+        Data4 = "";
+        int l = LEDs.toInt();
+        strip.setPixelColor(l, R, G, B);
+        LEDs = "";
+        l = 0;
+        Indicator();
+      }
+      else {
+        Serial.println("HTTP Not Working");
+
+      }
+       resolver.loop();
+       delay(1000);
     }
-    resolver.loop();
   }
 }
