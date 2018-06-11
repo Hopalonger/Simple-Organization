51e6640b4453348152cff0804d33ae3985ee5c6f
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-06-10 21:37:38 -0700

    Update Static.py

--- Server/Static.py
+++ Server/Static.py
@@ -1,3 +1,9 @@
+import socket
+import fcntl
+import struct
+import os
+import commands
+end = 161;
 def setIP_XXX(ip):
 
     os.system('sudo ifconfig eth0 down')
@@ -6,24 +12,10 @@ def setIP_XXX(ip):
 
     os.system('sudo ifconfig eth0 up')
     
-import socket
-import fcntl
-import struct
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
-    
-first = get_ip_address('eth0')
-if first[:3] > 100
-    ip = First[:2] + ".50.50.80"
-    setIP_XXX(ip)
-    print(ip)
+a = commands.getoutput('hostname -I')
+
+if a[-2;] = "."
+    a.replace(a[-1:],161)
 else:
-    ip = First[:1] + ".50.50.80"
-     setIP_XXX(ip)
-     print(ip)
+    a.replace(a[-2:],161)
