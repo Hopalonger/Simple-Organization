9a261a742d1be0c3c88d227d68f25f7b220e5ff0
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-06-15 14:53:49 -0700

    Update Connection.ino

--- Esp8266-code/Connection.ino
+++ Esp8266-code/Connection.ino
@@ -1,109 +1,79 @@
-#include <WiFiManager.h>
 
 #include <Adafruit_NeoPixel.h>
-
+#include <WiFiManager.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
-//******************************************************************************
-// Device Code
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+//*********************************************************
+// Which pin on the Arduino is connected to the NeoPixels for the Main Strip?
+#define PIN            15
+// How many NeoPixels are attached to the Arduino for the Main Strip?
+#define NUMPIXELS      60
+// Which pin on the Arduino is connected to the NeoPixels for the Indicator Strip?
+#define IND 8
+// How many NeoPixels are attached to the Arduino for the Indicator Strip?
+#define LND 
+// Ip Address of Server
+String  ipString = "10.42.252.179";
+// Device Code 4 Char Alpha Numerical
 String Code = "aaaa";
-// Server IP
-String  ipString = "10.42.252.161";
-//color
+//Color in RGB
 #define R 255
 #define G 255
 #define B 255
-// Number of LED's
-#define LED 60
-// Led Connected Pin
-#define PIN 6
-// Indicator Pin
-#define IND 8
-// Indicator LEDs
-#define LND 8
-//*******************************************************************************
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED, PIN, NEO_GRB + NEO_KHZ400);
-Adafruit_NeoPixel ind = Adafruit_NeoPixel(LND, IND, NEO_GRB + NEO_KHZ400);
-
+**************************************************************
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ind = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 WiFiClient wifiClient;
 WiFiUDP udp;
-
 IPAddress ip;
-void Indicator() {
-  int count = 0;
-  while (count < LND) {
-    ind.setPixelColor(count, 255, 255, 255);
-    count++;
-  }
-
-}
-void Start() {
-  int count = 0;
-  while (count < LND) {
-    strip.setPixelColor(count, 255, 255, 255);
-    count++;
-  }
-  count = 0;
-  while (count < LND) {
-    ind.setPixelColor(count, 255, 255, 255);
-    count++;
-  }
-  ind.show();
-
-  strip.show();
-  delay(5000);
-  count = 0;
-  while (count < LND) {
-    strip.setPixelColor(count, 0, 0, 0);
-    count++;
-  }
-
-  count = 0;
-  while (count < LND) {
-    ind.setPixelColor(count, 0, 0, 0);
-    count++;
-  }
-  ind.show();
-  strip.show();
-}
-
 void setup() {
   Serial.begin(115200);
-  int WiFiCounter = 0;
-  // We start by connecting to a WiFi network
-  Serial.println("Connecting to Internet. Connect Through Gatway.");
-
+  // Internet Connection With The Wifi Manager Libary
   WiFiManager wifiManager;
-
   wifiManager.autoConnect("Organization");
-  
- 
+  pixels.begin(); // This initializes the NeoPixel library.
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  // Test the connected LEDS with a Green Color On Boot
+  for (int i = 0; i < NUMPIXELS; i++) {
 
-  Serial.print("Resolving ");
-  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Moderately bright green color.
+
+    // Delay for a period of time (in milliseconds).
+
+  }
+  pixels.show(); // This sends the updated pixel color to the hardware.
+
+  delay(5000);
+  for ( int i = 0; i < NUMPIXELS; i++) {
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
+
+    // Delay for a period of time (in milliseconds).
+
+  }
+  pixels.show();
 }
-
 void loop() {
-  
-  // Required to clear the UDP buffer, as we can't unjoin a multicast group yet
+
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
   Serial.println("Running");
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     Serial.print("Connecting to:");
-
+    nhfg bc
     HTTPClient Check;
     HTTPClient http;  //De lare an object of class HTTPClient
-    
+
     String url = "http://" + ipString + "/newfile.txt";
     String Power = "http://" + ipString + "/power.txt";
     Serial.println(url);
@@ -113,11 +83,11 @@ void loop() {
     int httpCode = Check.GET();                                                                  //Send the request
 
     if (httpCode > 0) { //Check the returning code
-      String Client = Check.getString();
+      String C = Check.getString();
       Serial.println("Checking Server");
-      if (Client.toInt() > 100) {
+      Serial.println("Got: " + C);
+      if (C.toInt() > 100) {
         Check.end();
-        
         Serial.println("Recent");
         String url = "http://" + ipString + "/newfile.txt";
         http.begin(url);  //Specify request destination
@@ -127,35 +97,41 @@ void loop() {
         if (bad > 0) {
           Serial.println(url);
           String payload = http.getString();
-
           //Get the request response payload
           Serial.print("Got:");
           Serial.println(payload);    //Print the response payload
-          Serial.println("Test");
           if ( Code == payload.substring(0, 4)) {
             Serial.print("Got Code");
             Serial.println(payload.substring(0, 4));
             Serial.print("Got LED:");
             Serial.println(payload.substring(4));
             String light = payload.substring(4);
-            strip.setPixelColor(light.toInt(), R, G, B);
+            pixels.setPixelColor(light.toInt(), R, G, B);
+            pixels.show();
             Indicator();
             light = "";
-            delay(5000); 
+            delay(5000);
           }
           else {
-            Serial.println("Not THis Device");
+            Serial.println("Not This Device");
           }
         }
-        
-        }
-        else {
-          Serial.println("Not Recent");
-          Check.end();
       }
-
+      else {
+        Serial.println("Not Recent");
+        Check.end();
+      }
       http.end();   //Close connection
       delay(1000);
     }
   }
+}
+
+void Indicator() {
+  int count = 0;
+  while (count < LND) {
+    ind.setPixelColor(count, 255, 255, 255);
+    count++;
+  }
+ ind.show();
 }
