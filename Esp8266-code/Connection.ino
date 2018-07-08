09c5e13cd77429037d33d5d55f327402b7c19c1e
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2018-07-08 16:50:46 -0700

    Fixed Lights Not Turning off
    Added Pin Interupt to Turn Lights off when The Button is pushed

--- Esp8266-code/Connection.ino
+++ Esp8266-code/Connection.ino
@@ -6,16 +6,17 @@
 #include <ESP8266HTTPClient.h>
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WebServer.h>
+
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 //*********************************************************
 // Which pin on the Arduino is connected to the NeoPixels for the Main Strip?
-#define PIN            15
+#define PIN            2
 // How many NeoPixels are attached to the Arduino for the Main Strip?
-#define NUMPIXELS      10
+#define NUMPIXELS      11
 // Which pin on the Arduino is connected to the NeoPixels for the Indicator Strip?
-#define IND 5
+#define IND 0
 // How many NeoPixels are attached to the Arduino for the Indicator Strip?
 #define LND 6
 // Ip Address of Server (last 2 or 3 Digits of IP works best with static IP on the same subnet such as 10.42.252.179.
@@ -27,7 +28,8 @@ String Code = "aaaa";
 #define R 255
 #define G 0
 #define B 125
-
+// The Pin for Which The Switch is connected to
+const byte interruptPin = 15;
 //**************************************************************
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ind = Adafruit_NeoPixel(LND, IND, NEO_GRB + NEO_KHZ800);
@@ -39,7 +41,6 @@ String Ip;
 String Da;
 String page = "";
 int b = 0;
-
 
 String Count;
 String Stat;
@@ -62,11 +63,25 @@ void IndicatorOff () {
   String state = server.arg("state");
   for ( int i = 0; i < NUMPIXELS; i++) {
     ind.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
-    }
-    ind.show();
-    server.send(200, "text/html", " <center><title> Device Stat </title><h1>Organization Device Status</h1> <h3> The Indicator Lights Are now off </h3> <form action = '/'> <input type='submit' value='Go Back' /></form> ");
-  
+  }
+  ind.show();
+  server.send(200, "text/html", " <center><title> Device Stat </title><h1>Organization Device Status</h1> <h3> The Indicator Lights Are now off </h3> <form action = '/'> <input type='submit' value='Go Back' /></form> ");
+
 }
+void Clear() {
+  for ( int i = 0; i < NUMPIXELS; i++) {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // OFF
+    ind.setPixelColor(i, pixels.Color(0, 0, 0)); // OFF
+  }
+  pixels.show();
+  ind.show();
+}
+void handleInterrupt() {
+  Clear();
+  Serial.println("Lights Cleared, Internupt was incountered");
+}
+
 void setup() {
   Serial.begin(115200);
   // Internet Connection With The Wifi Manager Libary
@@ -90,38 +105,33 @@ void setup() {
   pixels.show(); // This sends the updated pixel color to the hardware.
   ind.show();
   delay(5000);
-  for ( int i = 0; i < NUMPIXELS; i++) {
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
-    ind.setPixelColor(i, pixels.Color(0, 0, 0)); // Moderately bright green color.
-    // Delay for a period of time (in milliseconds).
-
-  }
-  pixels.show();
-  ind.show();
+  Clear();
 
   server.on("/", []() {
+
     String Line1 = "<title> Esp Data</title><center> <h1>Sensor to Node MCU Web Server</h1> <h3>Server Checks:" + String(b) + "</h3> <h3> Status: " + String(Stat) + "</h3>  ";
-    String Line2 = "<h3><a href='/ledON'>Turn On The Indicator Lights</a></h3><h3><a href='/ledOff'>Turn Off The Indicator Lights</a></h3> <h3>";
+    String Line2 = "<h3><a href='/ledON'>Turn On The Inidcator Lights</a></h3><h3><a href='/ledOff'>Turn Off The Inidcator Lights</a></h3> <h3>";
     String Line3 = "<input type='button' value='Refresh Page' onClick='location.href=location.href'></h3> </center>";
     String page = Line1 + Line2 + Line3;
     server.send(200, "text/html", page );
   });
   server.on("/ledON", IndicatorON);
   server.on("/ledOff", IndicatorOff);
-  server.begin();    //Start server
+  server.begin();                  //Start server
   Serial.println("HTTP server started");
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING);
+  Serial.println("Interupt Attached");
 }
 
 void loop() {
 
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  
+  Serial.println("Running");
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     Serial.print("Connecting to:");
     HTTPClient Check;
-    HTTPClient http;  //Declare an object of class HTTPClient
+    HTTPClient http;  //De lare an object of class HTTPClient
     IPAddress ipstring = WiFi.localIP();
     Ip = String(ipstring[0]) + '.' + String(ipstring[1]) + '.' + String(ipstring[2]) + '.' + ipend ;
     String url = "http://" + Ip + "/access.txt";
@@ -157,17 +167,21 @@ void loop() {
             Stat = "Got Data" + D;
             while ( i < 7) {
               Serial.println(D);
-              Serial.print("Got LED's :");
+              Serial.print(D.charAt(4 - i * 2));
+              Serial.println(D.charAt(5 - i * 2));
               Da = String(D.charAt(4 - i * 2)) + String(D.charAt(5 - i * 2));
               D.remove(5 - i * 2);
-              Serial.print(Da + ",");
-              pixels.setPixelColor(Da.toInt(), R, G, B);
+              Serial.println(D);
+              Serial.print("String: ");
+              Serial.println(Da);
+              Serial.println("Done");
+              pixels.setPixelColor((Da.toInt() + 2) * 2, R, G, B);
               i++;
             }
             pixels.show();
             Indicator();
             Da = "";
-            Serial.println(" ");
+
             delay(5000);
           }
           else {
@@ -188,4 +202,6 @@ void loop() {
   b++;
   delay(100);
   server.handleClient();
+
 }
+
